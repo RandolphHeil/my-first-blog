abc53c5bda176940ff8c94028c3ec8a2a79541ab
RandolphHeil <randolphheil@gmx.de>
2017-04-26 16:11:58 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
+    post = Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
     return render(request, 'blog/post_list.html', {"post":post})
 
 
